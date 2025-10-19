2007a695f5cc88c0814b3813fc7c53e20ad220a6
Nishantt02 <nishantchauhannn@gmail.com>
2025-10-19 22:51:59 +0530

    deployment chnages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,21 +1,4 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-// import { UserProvider } from "./context/Usercontext.jsx";
-// import { ChatProvider } from "./context/Chatcontext.jsx";
 
-// export const server = "http://localhost:5000";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <UserProvider>
-//       <ChatProvider>
-//         <App />
-//       </ChatProvider>
-//     </UserProvider>
-//   </StrictMode>
-// );
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -23,7 +6,7 @@ import "./index.css";
 import { UserProvider } from "./context/UserContext.jsx"; // Exact match
 import { ChatProvider } from "./context/ChatContext.jsx"; // Exact match
 
-export const server = "http://localhost:5000";
+export const server = "https://fullstack-chatbott-4.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
